95b24f65b0e5acdf49cbf08aff64f07181c78e7f
Shin3ea <xinleeds@gmail.com>
2021-03-22 09:01:15 +0000

    Upadate

--- story/views.py
+++ story/views.py
@@ -149,13 +149,14 @@ def HandleGetStoriesRequest(request):
     for obj in obj_set:
       datelist=obj.Post_Date.isoformat().split('-')
       date=datelist[2]+'/'+datelist[1]+'/'+datelist[0]
-      if obj.Story_Details:
-        SomethingNone=("{username}").format(username=obj.Story_Details)
+      # if obj.Story_Details:
+      #   SomethingNone=("{username}").format(username=obj.Story_Details)
       # return HttpResponse(SomethingNone,content_type=text_content_type,status=403)
       # user_json=serializers.serialize("json",obj.Authors.Username)
       story_dict={"key":obj.id,"headline":obj.Story_Headline,
                   "story_cat":obj.Story_Category,"story_region":obj.Story_Region,
-                  "author":str(obj.Authors.Username),"story_details":obj.Story_Details}
+                  "author":str(obj.Authors.Username),"story_date"=date,
+                  "story_details":obj.Story_Details}
       # story_dict.update(user_json)
       all_story.append(story_dict)
       # json_response=serializers.serialize("json",all_story)
